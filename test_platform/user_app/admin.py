f42528c0796a010eeb54d9f1bfc051c0b9597ff7
ychen <284784606@qq.com>
2018-10-11 15:46:26 +0800

    创建项目和模块表

--- test_platform/user_app/admin.py
+++ test_platform/user_app/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
+from user_app.models import Project, Module
 
 # Register your models here.
 # django自带一个admin后台
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['name', 'describe', 'status', 'create_time', 'id']
+    search_fields = ['name']  # 搜索栏
+    list_filter = ['status']  # 过滤器
+
+
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ['name', 'describe', 'create_time', 'project', 'id']
+    search_fields = ['name']  # 搜索栏
+
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Module, ModuleAdmin)
--- test_platform/user_app/views.py
+++ test_platform/user_app/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib import auth
+from user_app.models import Project, Module
 # Create your views here.
 # 主要代码逻辑
 
@@ -37,7 +38,9 @@ def login_action(request):
 @login_required  # 判断用户是否登录
 def project_manage(request):
     username = request.session.get('user', '')  # 读取浏览器 session
-    return render(request, "project_manage.html", {"user": username})
+    project_all = Project.objects.all()
+    print(project_all)
+    return render(request, "project_manage.html", {"user": username, "projects": project_all})
 
 
 def logout(request):

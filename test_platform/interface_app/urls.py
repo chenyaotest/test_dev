a3e8826272cb70bb513b6d4cb92a5488cd80c23f
ychen <284784606@qq.com>
2018-11-06 09:52:40 +0800

    在线调试接口

--- test_platform/interface_app/urls.py
+++ test_platform/interface_app/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
 
     # 项目管理
     path('case_manage/', views.case_manage),
+    path('debug/', views.debug),
     path('api_debug/', views.api_debug),
-
 ]
--- test_platform/interface_app/views.py
+++ test_platform/interface_app/views.py
@@ -1,3 +1,5 @@
+import json
+import requests
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -12,10 +14,30 @@ def case_manage(request):
         return HttpResponse("404")
 
 
-def api_debug(request):
+def debug(request):
     if request.method == "GET":
         return render(request, "api_debug.html", {
             "type": "debug"
         })
     else:
         return HttpResponse("404")
+
+
+def api_debug(request):
+
+        if request.method == "POST":
+            url = request.POST.get("req_url")
+            method = request.POST.get("req_method")
+            parameter = request.POST.get("req_parameter")
+            print(url)
+            print(method)
+            print(parameter)
+            payload = json.loads(parameter.replace("'", "\""))
+
+            if method == "get":
+                r = requests.get(url, params=payload)
+
+            if method == "post":
+                r = requests.post(url, json=payload)
+            print(r.text)
+            return HttpResponse(r.text)
